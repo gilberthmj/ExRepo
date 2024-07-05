cb29f05d8e5f0a08192f162754ed1fbad9366880
GILBERTH MORENO JIMENEZ <Gilberth.Moreno@alu.uclm.es>
2024-07-05 18:22:11 +0200

    Agregado carpeta BASICOS + ejer1

---
+++ P1/Basicos/ejercicio1.py
@@ -0,0 +1,9 @@
+# Ejecicio básico 1: Mostrar la vesión actual de Python.
+
+import sys
+
+print(sys.version)  # Se imprime la version de python
+
+print("")
+
+print(sys.version_info)   # Se imprime de manera más detallada la info
